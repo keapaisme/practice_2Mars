276b3e7d423c55d5cfa3fef3875a40c7c5491e8a
kasumi <www.clean.com@gmail.com>
2018-06-17 03:00:43 +0800

    完成PROJECT ：只秀出 U1，U2 兩次任務的總預算。

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,39 +2,41 @@ package com.company;
 import java.io.File;
 import java.util.*;
 
-/**
- * 创建一个 Simulation 对象
- * 为第一阶段和第二阶段装载 Item
- * 为第一阶段和第二阶段装载 U1 火箭舰队
- * 使用 U1 火箭舰队运行模拟，并显示所需的总预算。
- * 为 U2 火箭重复相同的流程，并显示所需的总预算。
- */
-public class Main {
 
+public class Main {
+    /**
+     * 创建一个 Simulation 对象
+     * 为第一阶段和第二阶段装载 Item
+     * 为第一阶段和第二阶段装载 U1 火箭舰队
+     * 使用 U1 火箭舰队运行模拟，并显示所需的总预算。
+     * 为 U2 火箭重复相同的流程，并显示所需的总预算。
+     * @param totalU1 U1,2階段任務總預算
+     * @param totalU2 U2,2階段任務總預算
+     */
     public static void main(String[] args)throws Exception {
-	// write your code here
+	// 讀入2次任務待運清單
         Simulation mission1 = new Simulation();
         Simulation mission2 = new Simulation();
         mission1.loadItems("phase-1.txt");
         mission2.loadItems("phase-2.txt");
-
+    //計算U1火箭執行2次任務所需預算
         System.out.println("\n計算U1火箭執行任務");
         mission1.loadU1(new U1());
         mission2.loadU1(new U1());
         long totalU1 = mission1.runSimulation(new U1())+mission2.runSimulation(new U1());
+    //計算U2火箭執行2次任務所需預算
         System.out.println("\n計算U2火箭執行任務");
         mission1.loadU2(new U2());
         mission2.loadU2(new U2());
         long totalU2 = mission1.runSimulation(new U2())+mission2.runSimulation(new U2());
-
+    //顯示U1，U2所需的總預算
         System.out.println("+++++++++++++++++++++++++++++++++++");
         System.out.println("U1 執行任務總預算為 $"+totalU1+" 美元。");
         System.out.println("U2 執行任務總預算為 $"+totalU2+" 美元。");
-
     }
 }
 
-//tem類含火箭要携帶的物品及其重量
+//item類含火箭要携帶的物品及其重量
 class Item{
     int weight ;
     String name;
@@ -58,7 +60,7 @@ class Item{
     }
 }
 
-//SpaceShip
+//SpaceShip:
 interface SpaceShip {
     boolean launch(int temp);
     boolean land(int temp);
